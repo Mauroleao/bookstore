a6a44eff206cc2cc9a6dcb618927d094c9e718fb
Mauro <your.email@example.com>
2025-01-22 08:59:08 -0300

    criado as pastas e seus respectivos arquivos de serializers

---
+++ product/models/product.py
@@ -0,0 +1,9 @@
+from unicodedata import category
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    descvription = models.TextField(max_length=500, blank=True, null=True)
+    price = models.PositiveBigIntegerField(null=True)
+    active = models.BooleanField(default=True)
+    categories = models.ManyToManyField(category, blank=True)
